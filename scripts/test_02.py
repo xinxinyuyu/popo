6493d6cfe91e8029b352a0176ca2b3155fb8e0e2
gg <gggg126.com>
2019-01-24 22:42:05 +0800

    werf

---
+++ scripts/test_02.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import allure
+
+@allure.step(title="测试登录的流程")
+def test_login():
+    allure.attach("登录", "输入用户名")
+    print("b")
+
+    allure.attach("登录", "输入密码")
+    print("a")
+
+    allure.attach("登录", "点击登录")
+    print("fff")
+
+    assert 1
+
