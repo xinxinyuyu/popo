ed2ef3c820289cae896c4bbd99b91d0330229967
xinxinyuyu <46963922+xinxinyuyu@users.noreply.github.com>
2019-01-24 23:07:40 +0800

    Update test_02.py

--- scripts/test_02.py
+++ scripts/test_02.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-import allure
+# import allure
 
-@allure.step(title="测试登录的流程")
+# @allure.step(title="测试登录的流程")
 def test_login():
-    allure.attach("登录", "输入用户名")
+#     allure.attach("登录", "输入用户名")
     print("b")
 
-    allure.attach("登录", "输入密码")
+#     allure.attach("登录", "输入密码")
     print("a")
 
-    allure.attach("登录", "点击登录")
+#     allure.attach("登录", "点击登录")
     print("fff")
 
     assert 1
